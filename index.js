737152adce42ea6ba47fbd1697f4f22be67d6aaf
DenisRaymer <to.mars.66.miles@gmail.com>
2020-07-27 19:40:17 +0300

    Created started page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const {createServer} = require('http');
 const url = 'mongodb+srv://dbUser:dbuser@cluster0.ueslo.mongodb.net/captain-quack?retryWrites=true&w=majority';
-const port = 4000;
+const port = process.env.PORT || 4000;
 const app = express();
 
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
@@ -12,6 +12,10 @@ mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use(bodyParser.json());
 app.use('/api', require('./api'));
+
+app.get('/', (request, response) => {
+    response.end('<h1>Hello world</h1>')
+});
 
 const server = createServer(app);
 server.listen(port, () => console.log(`Server is up. port: ${port}`));
