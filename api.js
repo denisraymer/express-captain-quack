54b4ce9631d85815b11660e886a9e5d4aebb40dc
DenisRaymer <to.mars.66.miles@gmail.com>
2020-07-28 14:35:07 +0300

    Fix cors function

--- api.js
+++ api.js
@@ -1,28 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const Mugger = require('./mugger');
-const cors = require('cors');
 
-const corsOptions = {
-    origin: 'https://denisraimer.github.io',
-    optionsSuccessStatus: 200
-}
-
-router.get('/products', cors(corsOptions), (request, response) => {
+router.get('/products', (request, response) => {
     Mugger.find({})
         .then(mugger => {
             response.send(mugger)
         })
 });
 
-router.post('/products', cors(corsOptions), (request, response) => {
+router.post('/products', (request, response) => {
     Mugger.create(request.body)
         .then(mugger => {
             response.send(mugger);
         });
 });
 
-router.put('/products/:id', cors(corsOptions), (request, response) => {
+router.put('/products/:id', (request, response) => {
     Mugger.findByIdAndUpdate({_id: request.params.id}, request.body)
         .then(() => {
             Mugger.findOne({_id: request.params.id})
@@ -32,7 +26,7 @@ router.put('/products/:id', cors(corsOptions), (request, response) => {
         });
 });
 
-router.delete('/products/:id', cors(corsOptions), (request, response) => {
+router.delete('/products/:id', (request, response) => {
     Mugger.deleteOne({_id: request.params.id})
         .then(mugger => {
             response.send(mugger);
